43b68302108108b758d193c0c8e854e33d2885cd
luislorente13 <119288518+luislorente13@users.noreply.github.com>
2023-11-13 18:12:06 +0100

    Update script.js

--- TresEnRaya/JS/script.js
+++ TresEnRaya/JS/script.js
@@ -1,17 +1,36 @@
 
+// pedir usuario
 document.addEventListener('DOMContentLoaded', function () {
+
+    do {
+        usuario = prompt("Introduce el nombre de usuario");
+        if (!isNaN(usuario) || usuario === undefined) {
+
+            alert("ERROR. Nombre de usuario no válido");
+        } else {
+
+            document.getElementById("bienvenida").innerHTML = "Bienvenido, " + usuario;
+            break;
+        }
+    } while (true);
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // tablero
     const menu = document.getElementById('menu');
     const tablero = document.getElementById('tablero');
 
     const jugarBtn = document.getElementById('jugarBtn');
     const estadisticasBtn = document.getElementById('estadisticasBtn');
 
-    jugarBtn.addEventListener('click', function () {
-        // Iniciar la lógica del juego cuando se haga clic en "Jugar Partida"
+    jugarBtn.addEventListener('click', function () { // ejecuta la funcion 'iniciarJuego()'
+        
         iniciarJuego();
     });
 
-    // Lógica del juego
+    // iniciarJuego()
     function iniciarJuego() {
         // Puedes implementar aquí la lógica del juego de Tres en Raya
         // Por ejemplo, mostrar el tablero y gestionar los clics en las casillas
