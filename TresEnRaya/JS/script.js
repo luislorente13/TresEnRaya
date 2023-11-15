fad8a9b43649d62ba03fc9a763b209d23c374615
luislorente13 <119288518+luislorente13@users.noreply.github.com>
2023-11-15 15:32:55 +0100

    Update script.js

--- TresEnRaya/JS/script.js
+++ TresEnRaya/JS/script.js
@@ -44,3 +44,39 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('Ver estadísticas');
     });
 });
+
+
+            celdasNoMarcadas.push(i);
+        }
+    });
+
+    if (celdasNoMarcadas.length > 0) {
+        var indiceAleatorio = celdasNoMarcadas[Math.floor(Math.random() * celdasNoMarcadas.length)];
+        
+        var celdaAleatoria = celdas[indiceAleatorio];
+        celdaAleatoria.innerHTML = "O";
+    } else if (celdasNoMarcadas.length <= 0){
+        alert ("Hubo un empate");
+    }
+}
+
+// comprobar ganador
+function comprobarGanador(){
+    if(c1.textContent === "X" && c2.textContent === "X" && c3.textContent === "X") alert("El jugador a ganado");
+    else if(c4.textContent === "X" && c5.textContent === "X" && c6.textContent === "X") alert("El jugador a ganado");
+    else if(c7.textContent === "X" && c8.textContent === "X" && c9.textContent === "X") alert("El jugador a ganado");
+    else if(c1.textContent === "X" && c4.textContent === "X" && c7.textContent === "X") alert("El jugador a ganado");
+    else if(c2.textContent === "X" && c5.textContent === "X" && c8.textContent === "X") alert("El jugador a ganado");
+    else if(c3.textContent === "X" && c6.textContent === "X" && c9.textContent === "X") alert("El jugador a ganado");
+    else if(c1.textContent === "X" && c5.textContent === "X" && c9.textContent === "X") alert("El jugador a ganado");
+    else if(c7.textContent === "X" && c5.textContent === "X" && c3.textContent === "X") alert("El jugador a ganado");
+
+    else if(c4.textContent === "O" && c5.textContent === "O" && c6.textContent === "O") alert("El ordenador a ganado");
+    else if(c7.textContent === "O" && c8.textContent === "O" && c9.textContent === "O") alert("El ordenador a ganado");
+    else if(c1.textContent === "O" && c4.textContent === "O" && c7.textContent === "O") alert("El ordenador a ganado");
+    else if(c2.textContent === "O" && c5.textContent === "O" && c8.textContent === "O") alert("El ordenador a ganado");
+    else if(c3.textContent === "O" && c6.textContent === "O" && c9.textContent === "O") alert("El ordenador a ganado");
+    else if(c1.textContent === "O" && c5.textContent === "O" && c9.textContent === "O") alert("El ordenador a ganado");
+    else if(c7.textContent === "O" && c5.textContent === "O" && c3.textContent === "O") alert("El ordenador a ganado");
+    else if(c7.textContent === "O" && c5.textContent === "O" && c3.textContent === "O") alert("El ordenador a ganado");
+}
